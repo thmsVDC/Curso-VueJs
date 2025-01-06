801b6195771150a8d6aaf9f88972440141af2734
thmsVDC <thomasvictor2909@gmail.com>
2025-01-06 20:57:42 -0300

    Hierarquia de componentes

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,18 +3,21 @@
     <PrimeiroComponente/>
     <p>o dado de app é {{ testing }}</p>
     <LifeCycle/>
+    <Pessoa/>
   </div>
 </template>
 
 <script>
   import PrimeiroComponente from './components/PrimeiroComponente.vue';
   import LifeCycle from './components/LifeCycle.vue';
+  import Pessoa from './components/Pessoa.vue';
 
   export default {
     name: "App",
     components: {
       PrimeiroComponente,
-      LifeCycle
+      LifeCycle,
+      Pessoa
     },
     data () {
       return {
---
+++ vue-project/src/components/Info.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>Nome: {{ nome }}</p>
+    <p>Estou trabalhando no momento.</p>
+    <p>Utilizo as seguintes tecnologias:</p>
+    <ul>
+      <li>Python</li>
+      <li>JavaScript</li>
+      <li>Java</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Info",
+    data() {
+      return {
+        nome: "Thomas"
+      }
+    }
+
+  }
+</script>
