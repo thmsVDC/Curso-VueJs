684ec36831a1d982411db21153a7fe8ccd360a41
thmsVDC <thomasvictor2909@gmail.com>
2025-01-06 19:35:27 -0300

    LifeCycle

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,18 +2,22 @@
   <div>
     <PrimeiroComponente/>
     <p>o dado de app é {{ testing }}</p>
+    <LifeCycle/>
   </div>
 </template>
 
 <script>
   import PrimeiroComponente from './components/PrimeiroComponente.vue';
+  import LifeCycle from './components/LifeCycle.vue';
+
   export default {
     name: "App",
     components: {
-      PrimeiroComponente
+      PrimeiroComponente,
+      LifeCycle
     },
-    data() {
-      return{
+    data () {
+      return {
         testing: "test"
       }
     }
---
+++ vue-project/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+  export default {
+    name: "LifeCycle",
+    data() {
+      return {
+        nome: "Ainda não sei"
+      }
+    },
+    created() {
+      setTimeout(() => {
+        this.nome = "Thomas Victor"
+      }, 1000);
+    },
+    mounted() {
+      setTimeout(() => {
+        this.nome = "Thomas Victor Dias Carvalho"
+      }, 2000);
+    }
+  }
+</script>
